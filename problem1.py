96281b8dc20bc4439cb4a8a02f24f7d6328254a5
LekhanaWhat <54971871+LekhanaWhat@users.noreply.github.com>
2020-02-26 17:47:10 +0530

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,6 @@
+name=input("enter your name")
+age=int(input("enter your age"))
+current_year=2020
+birth_year=current_year-age
+fage=100+birth_year
+print(f"hey {name}! You'll turn 100 years old in {fage}")
